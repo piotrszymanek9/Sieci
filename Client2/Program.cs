fbd63f716b1236019d5bb30a558f35d7353123e3
Piotr Szymanek <szymanekpiotr9@gmail.com>
2017-05-07 18:12:04 +0200

    Init

---
+++ Client2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NetworkCommsDotNet;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wpisz ip w formacie 192.168.0.1:10000");
+            string serverInfo = Console.ReadLine();
+            string serverIP = serverInfo.Split(':').First();
+            int serverPort = int.Parse(serverInfo.Split(':').Last());
+            while (true)
+            {
+                //wiadomosc
+                string messageToSend = Console.ReadLine();
+                Console.WriteLine("Wiadomosc:" + messageToSend);
+                NetworkComms.SendObject("Wiadomosc", serverIP, serverPort, messageToSend);
+                Console.WriteLine("q- zeby wyjsc, nowa wiadomosc");
+                if (Console.ReadKey(true).Key == ConsoleKey.Q) break;
+            }
+            NetworkComms.Shutdown();
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NetworkCommsDotNet;
+using NetworkCommsDotNet.Connections;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            NetworkComms.AppendGlobalIncomingPacketHandler<string>("Wiadomosc", writeMSG);
+            Connection.StartListening(ConnectionType.TCP, new System.Net.IPEndPoint(System.Net.IPAddress.Any, 0));
+
+            //Wyswietlenie nasluchiwanych adresow
+            Console.WriteLine("Nasluchiwane adresy:");
+            foreach (System.Net.IPEndPoint localEndPoint in Connection.ExistingLocalListenEndPoints(ConnectionType.TCP))
+            {
+                Console.WriteLine("{0}:{1}", localEndPoint.Address, localEndPoint.Port);
+                }
+            Console.WriteLine("Nacisnij dowolny klawisz, aby wyjsc");
+            Console.ReadKey(true);
+            NetworkComms.Shutdown();
+        }
+        private static void writeMSG(PacketHeader header, Connection connection, string message)
+        {
+            Console.WriteLine(connection.ToString()+"Nowa wiadomosc: " + message );
+        }
+    }
+}
